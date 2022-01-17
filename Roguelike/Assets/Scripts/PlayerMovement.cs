d996c65dbc67c41e2a9e1bcfc9e4bea456d5bf43
ProfessorQu <quint@dasookwat.com>
2022-01-17 18:20:34 +0100

    Added bombs and improved jump
    Added destructible tag.
    Added bombs.
    Added coyote time and jump buffering.

--- Roguelike/Assets/Scripts/PlayerMovement.cs
+++ Roguelike/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
 
+    [Header("Ground Check")]
     public Transform groundCheck;
     public LayerMask whatIsGround;
     public float groundCheckRadius = 0.2f;
@@ -14,9 +15,22 @@ public class PlayerMovement : MonoBehaviour
     float horizontal;
     bool facingRight = true;
 
+    [Header("Speed")]
     public float speed = 8f;
+
+    public float bombRadius = 10f;
+
+    [Header("Jump")]
+    public float rememberGroundedTime = 0.1f;
+    private float groundedTimer;
+
+    public float rememberJumpInputTime = 0.1f;
+    private float rememberJumpTimer;
+
     public float jumpForce = 16f;
     public float jumpEndedFallSpeed = 0.5f;
+
+    private bool jumpCanceled;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -32,6 +46,23 @@ public class PlayerMovement : MonoBehaviour
         else if (facingRight && horizontal < 0f) {
             Flip();
         }
+
+        if (IsGrounded()) {
+            groundedTimer = rememberGroundedTime;
+        }
+
+        if (groundedTimer > 0f && rememberJumpTimer > 0f) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            groundedTimer = 0f;
+            rememberJumpTimer = 0f;
+        }
+        else if (jumpCanceled) {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpEndedFallSpeed);
+            jumpCanceled = false;
+        }
+
+        groundedTimer -= Time.deltaTime;
+        rememberJumpTimer -= Time.deltaTime;
     }
 
     private bool IsGrounded() {
@@ -50,12 +81,31 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public void Jump(InputAction.CallbackContext context) {
-        if (context.performed && IsGrounded()) {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (context.performed) {
+            rememberJumpTimer = rememberJumpInputTime;
         }
 
         if (context.canceled && rb.velocity.y > 0f) {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpEndedFallSpeed);
+            jumpCanceled = true;
+        }
+
+        // if (context.performed && IsGrounded()) {
+        //     rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        // }
+
+        // if (context.canceled && rb.velocity.y > 0f) {
+        //     rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpEndedFallSpeed);
+        // }
+    }
+
+    public void Bomb(InputAction.CallbackContext context) {
+        if (context.started) {
+            Collider2D[] blocks = Physics2D.OverlapCircleAll(transform.position, bombRadius);
+            foreach (var block in blocks) {
+                if (block.CompareTag("Destructible")) {
+                    Destroy(block.gameObject);
+                }
+            }
         }
     }
 }
